2fee96dcad974f66be485fb8b00b89e0f8dcd4ec
Vladimir Smolskiy <psihozykt@gmail.com>
2021-07-06 19:37:02 +0900

    added logout button

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -13,8 +13,6 @@ const Navbar = () => {
     return (
         <nav>
             <div className="nav-wrapper teal lighten-2">
-
-
                 <button className="waves-effect waves-light btn blue-grey darken-1" onClick={logoutHandler}> logout
                 </button>
             </div>
